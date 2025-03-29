90254bedf25bd7e1c1510651af3491f8c716e703
PanchalKeyur0806 <panchalkeyur694@outlook.com>
2025-03-29 14:51:34 +0530

    add css in login page

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 // global middlewares
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
