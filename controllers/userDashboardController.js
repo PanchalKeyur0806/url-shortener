ddeaf6ca07d045c5ef65676dbacae6b9268eb640
PanchalKeyur0806 <panchalkeyur694@outlook.com>
2025-04-27 15:06:28 +0530

    display the user dashboard with urls

--- controllers/userDashboardController.js
+++ controllers/userDashboardController.js
@@ -1,7 +1,36 @@
-const renderDashboard = (req, res) => {
+// installed models
+import moment from "moment-timezone";
+// custom modules
+import Url from "../models/urlModel.js";
+import AppError from "../utils/appError.js";
+import catchAsync from "../utils/catchAsync.js";
+
+// render user dashboard
+const renderDashboard = catchAsync(async (req, res) => {
+  // get the logged in user
+  const currentUser = req.user;
+  // find the urls according to the users
+  const usersURl = await Url.find({ userId: currentUser });
+  if (!usersURl) {
+    return next(new AppError("There is no url created by that user", 404));
+  }
+
+  // return a array of objects with ist timezone
+  const urlWithIst = usersURl.map((item) => {
+    return {
+      ...item._doc,
+      createdAtIst: moment(item.createdAt)
+        .tz("Asia/Kolkata")
+        .format("DD-MM-YYYY hh:mm A"),
+    };
+  });
+
+  // render the website
   res.render("userDashboard/dashboard", {
     title: "user dashboard - url shortener",
+    data: urlWithIst,
+    req,
   });
-};
+});
 
 export { renderDashboard };
--- routes/userDashboardRoutes.js
+++ routes/userDashboardRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { renderDashboard } from "../controllers/userDashboardController.js";
+import { protect } from "../middleware/protect.js";
 
 const router = express.Router();
 
-router.get("/", renderDashboard);
+router.get("/", protect, renderDashboard);
 
 export default router;
